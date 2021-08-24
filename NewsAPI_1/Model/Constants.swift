51d21be75f66943a63cf9e310796dc51d4d6a5d1
ramgangadhark <ramgangadhar_k@outlook.com>
2021-08-24 11:55:45 +0530

    Removed API Key

--- NewsAPI_1/Model/Constants.swift
+++ NewsAPI_1/Model/Constants.swift
@@ -8,12 +8,12 @@
 import Foundation
 struct Constants {
     static let news_API:String = "https://newsapi.org/v2/top-headlines?country=in&"
-    static let news_API_Key:String = "91fa59c60c364db2a2379d271a36541d"
+    static let news_API_Key:String = "YOUR_API_KEY"
     static let covid_API:String = "https://corona.lmao.ninja/v3/covid-19/countries/india"
     static let category = ["business":"business","entertainment":"entertainment","general":"general","health":"health","science":"science","sports":"sports","technology":"technology"]
     static let categoryArray = ["business","entertainment","general","health","science","sports","technology"]
     static let categoryArray1 = ["Business","Entertainment","General","Health","Science","Sports","Technology"]
 }
 //business entertainment general health science sports technology
-//91fa59c60c364db2a2379d271a36541d
+//
 //https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=
